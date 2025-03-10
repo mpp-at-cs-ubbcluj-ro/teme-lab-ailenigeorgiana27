517e1795dd42a087c60c32a0e30f183d8d22f096
Georgiana <ailenigeorgiana27@icloud.com>
2025-03-10 11:28:41 +0200

    Lab2CS

---
+++ csL2/Domain/Participant.cs
@@ -0,0 +1,18 @@
+﻿namespace lab2_mpp.domain;
+
+public class Participant
+{
+    public String Name { get; set; }
+    public int Age { get; set; }
+    public int NrProbe { get; set; }
+
+    public Participant(String name, int age, int nrProbe)
+    {
+        Name = name;
+        Age = age;
+        NrProbe = nrProbe;
+    }
+    
+    public override string ToString()
+    =>$"Participant {{ name: {Name}, age: {Age}, nrProbe: {NrProbe}}}";
+}
