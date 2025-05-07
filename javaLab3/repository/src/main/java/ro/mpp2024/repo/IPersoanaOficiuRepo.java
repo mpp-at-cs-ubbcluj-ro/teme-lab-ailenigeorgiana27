07c073c2b60d90f7aca37d59fd7262719cfe0855
Georgiana-Constantina Aileni <156085841+ailenigeorgiana27@users.noreply.github.com>
2025-05-07 22:21:40 +0300

    Delete javaLab3 directory

--- javaLab3/repository/src/main/java/ro/mpp2024/repo/IPersoanaOficiuRepo.java
+++
@@ -1,7 +0,0 @@
-package ro.mpp2024.repo;
-
-import ro.mpp2024.domain.PersoanaOficiu;
-
-public interface IPersoanaOficiuRepo extends IRepo<Integer, PersoanaOficiu> {
-    PersoanaOficiu findbyCredentials(String orasOficiu, String username, String password) throws EntityRepoException;
-}
